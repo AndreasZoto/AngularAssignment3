0fe5d3105d79b340d8693018e9c960dfe5438ad4
andrea zoto <andreazoto10@gmail.com>
2024-03-26 19:20:14 +0100

    Finish showing a list of Recipes in the view

--- src/app/features/recipes/recipe-item/recipe-item.component.ts
+++ src/app/features/recipes/recipe-item/recipe-item.component.ts
@@ -9,6 +9,10 @@ import {RecipeModel} from "../../../shared/models/recipe.model";
 export class RecipeItemComponent {
 
   recipes: RecipeModel[] = [
-    {name: 'Recipe One', description: 'Some description for Recipe One', imgUrl: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'}
+    {name: 'Recipe One', description: 'Some description for Recipe One', imgUrl: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'},
+    {name: 'Recipe Two', description: 'Some description for Recipe Two', imgUrl: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'},
+    {name: 'Recipe Three', description: 'Some description for Recipe Three', imgUrl: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'},
+    {name: 'Recipe Four', description: 'Some description for Recipe Four', imgUrl: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'},
+    {name: 'Recipe Five', description: 'Some description for Recipe Five', imgUrl: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'}
   ];
 }
